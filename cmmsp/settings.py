7216eeb475573cc9fae5236ff5d4a2221ddae27c
dawittamene <dawittamene559@gmail.com>
2023-11-26 21:49:12 +0300

    Minor Update

--- cmmsp/settings.py
+++ cmmsp/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # pip install python-decouple
 # Application definition
